5841e3c0277058c1cdc9c3394e2ecc5834199ab0
Axloon <ax.rojasc@duocuc.cl>
2024-05-18 04:56:08 -0400

    modelos-sqlite

--- ferremax/models.py
+++ ferremax/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.nombre
+    
+class Producto(models.Model):
+    nombre = models.CharField(max_length=50)
+    descripcion = models.TextField()
+    precio = models.IntegerField()
+    disponible = models.BooleanField()
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT)
+    imagen = models.ImageField(upload_to="productos", null=True)
+    
+    def __str__(self):
+        return self.nombre
